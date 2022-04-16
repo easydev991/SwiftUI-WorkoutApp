48156b1c68a7192f8ef9850300cc75c6db4d14fe
Oleg991 <o.n.eremenko@gmail.com>
2022-04-16 15:27:55 +0300

    Initial Commit

---
+++ SwiftUI-WorkoutApp/SwiftUI_WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WorkoutAppApp.swift
+//  SwiftUI-WorkoutApp
+//
+//  Created by Олег Еременко on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
