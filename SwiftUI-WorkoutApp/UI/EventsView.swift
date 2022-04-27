17ad98890f91435bca7d8f643ff1454811919c4f
Oleg991 <o.n.eremenko@gmail.com>
2022-04-27 20:20:34 +0300

    Work in progress
    Добавил заглушку об отсутствии мероприятий

--- SwiftUI-WorkoutApp/UI/EventsView.swift
+++ SwiftUI-WorkoutApp/UI/EventsView.swift
@@ -11,15 +11,34 @@ struct EventsView: View {
     var body: some View {
         NavigationView {
 #warning("TODO: интеграция с сервером")
-#warning("TODO: сверстать экран с мероприятиями и заглушку")
-            Text("Мероприятия")
+#warning("TODO: сверстать экран с мероприятиями")
+            dummyView()
                 .navigationTitle("Мероприятия")
         }
+    }
+}
+
+private extension EventsView {
+    func dummyView() -> some View {
+        VStack {
+            Text("Нет запланированных мероприятий")
+                .font(.title.weight(.medium))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+                .padding()
+            Image(systemName: "calendar")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.secondary.opacity(0.5))
+                .frame(width: 200)
+        }
+        .padding()
     }
 }
 
 struct EventsView_Previews: PreviewProvider {
     static var previews: some View {
         EventsView()
+            .previewDevice("iPhone 13 mini")
     }
 }
