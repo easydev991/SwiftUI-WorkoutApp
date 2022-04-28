d710db9b285f090479e682ef2e577b7f1054d144
Oleg991 <o.n.eremenko@gmail.com>
2022-04-28 21:48:12 +0300

    Work in progress
    Добавил переключатель типов мероприятий на первый экран с заглушкой

--- SwiftUI-WorkoutApp/UI/EventsView.swift
+++ SwiftUI-WorkoutApp/UI/EventsView.swift
@@ -7,22 +7,58 @@
 
 import SwiftUI
 
+enum EventType: String, CaseIterable {
+    case future = "Планируемые"
+    case past = "Прошедшие"
+}
+
 struct EventsView: View {
+    @State private var selectedEventType = EventType.future
+
     var body: some View {
         NavigationView {
 #warning("TODO: интеграция с сервером")
 #warning("TODO: сверстать экран со списком мероприятий")
-            dummyView()
-                .navigationTitle("Мероприятия")
+            VStack {
+                segmentedControl()
+                content()
+            }
+            .navigationTitle("Мероприятия")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 private extension EventsView {
+    func segmentedControl() -> some View {
+        Picker("Тип мероприятия", selection: $selectedEventType) {
+            ForEach(EventType.allCases, id: \.self) {
+                Text($0.rawValue)
+            }
+        }
+        .pickerStyle(.segmented)
+        .padding()
+    }
+
+    func content() -> AnyView {
+        switch selectedEventType {
+        case .future: return AnyView(dummyView())
+        case .past: return AnyView(eventsList())
+        }
+    }
+
+#warning("TODO: убрать временный хардкод после реализации фичи")
+    func eventsList() -> some View {
+        List(0..<15) { id in
+            Text("Тренировка № \(id)")
+        }
+    }
+
     func dummyView() -> some View {
         VStack(spacing: 16) {
+            Spacer()
             Text("Нет запланированных мероприятий")
-                .font(.title)
+                .font(.title2)
                 .multilineTextAlignment(.center)
             Image(systemName: "calendar")
                 .resizable()
@@ -38,6 +74,7 @@ private extension EventsView {
                 Text("Создать мероприятие")
                     .roundedBorderedStyle()
             }
+            Spacer()
         }
         .padding()
     }
@@ -46,6 +83,7 @@ private extension EventsView {
 struct EventsView_Previews: PreviewProvider {
     static var previews: some View {
         EventsView()
+            .preferredColorScheme(.dark)
             .previewDevice("iPhone 13 mini")
     }
 }
