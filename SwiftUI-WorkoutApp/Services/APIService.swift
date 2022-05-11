b9ef7ab9f8b9219239c282e424da931ea6d7e8a9
Oleg <51740599+OlegEremenko991@users.noreply.github.com>
2022-05-11 11:29:19 +0300

    Рефактор
    Добавил комментарии

--- SwiftUI-WorkoutApp/Services/APIService.swift
+++ SwiftUI-WorkoutApp/Services/APIService.swift
@@ -36,7 +36,7 @@ struct APIService {
         try await getUserByID(loginResponse.userID)
     }
 
-    /// Запрашивает данные пользователя по `id`
+    /// Запрашивает данные пользователя по `id`, сохраняет данные главного пользователя в `defaults` и авторизует, если еще не авторизован
     /// - Parameter userID: `id` пользователя
     /// - Returns: Вся информация о пользователе
     @discardableResult
@@ -54,6 +54,9 @@ struct APIService {
         return userInfo
     }
 
+    /// Сбрасывает пароль для неавторизованного пользователя с указанным логином
+    /// - Parameter login: `login` пользователя
+    /// - Returns: `true` в случае успеха, `false` при ошибках
     func resetPassword(for login: String) async throws -> Bool {
         let endpoint = Endpoint.resetPassword(login: login)
         guard let request = endpoint.urlRequest else { return false }
@@ -62,6 +65,11 @@ struct APIService {
         return userIdResponse.userID != .zero
     }
 
+    /// Меняет текущий пароль на новый, в случае успеха сохраняет новый пароль в `defaults`
+    /// - Parameters:
+    ///   - current: текущий пароль
+    ///   - new: новый пароль
+    /// - Returns: `true` в случае успеха, `false` при ошибках
     func changePassword(current: String, new: String) async throws -> Bool {
         let authData = defaults.basicAuthInfo
         let endpoint = Endpoint.changePassword(
@@ -76,6 +84,9 @@ struct APIService {
         return isSuccess
     }
 
+    /// Загружает список друзей для выбранного пользователя; для главного пользователя в случае успеха сохраняет идентификаторы друзей в `defaults`
+    /// - Parameter id: `id` пользователя
+    /// - Returns: список друзей выбранного пользователя
     @discardableResult
     func getFriendsForUser(id: Int) async throws -> [UserResponse] {
         let endpoint = Endpoint.getFriendsForUser(id: id, auth: defaults.basicAuthInfo)
@@ -90,6 +101,7 @@ struct APIService {
         return friends
     }
 
+    /// Загружает список заявок на добавление в друзья, в случае успеха - сохраняет в `defaults`
     func getFriendRequests() async throws {
         let endpoint = Endpoint.getFriendRequests(auth: defaults.basicAuthInfo)
         guard let request = endpoint.urlRequest else { return }
@@ -98,6 +110,11 @@ struct APIService {
         await defaults.saveFriendRequests(result)
     }
 
+    /// Отвечает на заявку для добавления в друзья, и в случае успеха запрашивает список заявок повторно, а если запрос одобрен - дополнительно запрашивает список друзей
+    /// - Parameters:
+    ///   - userID: `id` инициатора заявки
+    ///   - accept: `true` - одобрить заявку, `false` - отклонить
+    /// - Returns: `true` в случае успеха, `false` при ошибках
     func respondToFriendRequest(from userID: Int, accept: Bool) async throws -> Bool {
         let endpoint: Endpoint = accept
         ? .acceptFriendRequest(from: userID, auth: defaults.basicAuthInfo)
@@ -115,6 +132,11 @@ struct APIService {
         return isSuccess
     }
 
+    /// Совершает действие со статусом друга/пользователя
+    /// - Parameters:
+    ///   - userID: `id` пользователя, к которому применяется действие
+    ///   - option: вид действия - отправить заявку на добавление в друзья или удалить из списка друзей
+    /// - Returns: `true` в случае успеха, `false` при ошибках
     func friendAction(userID: Int, option: Constants.FriendAction) async throws -> Bool {
         let endpoint: Endpoint = option == .sendFriendRequest
         ? .sendFriendRequest(to: userID, auth: defaults.basicAuthInfo)
@@ -131,6 +153,9 @@ struct APIService {
         return isSuccess
     }
 
+    /// Ищет пользователей, чей логин содержит указанный текст
+    /// - Parameter name: текст для поиска
+    /// - Returns: список пользователей, чей логин содержит указанный текст
     func findUsers(with name: String) async throws -> [UserResponse] {
         let endpoint = Endpoint.findUsers(with: name, auth: defaults.basicAuthInfo)
         guard let request = endpoint.urlRequest else { return [] }
@@ -138,6 +163,9 @@ struct APIService {
         return try handle([UserResponse].self, data, response)
     }
 
+    /// Загружает данные по отдельной площадке
+    /// - Parameter id: `id` площадки
+    /// - Returns: данные о площадке
     func getSportsGround(id: Int) async throws -> SportsGround {
         let endpoint = Endpoint.getSportsGround(id: id, auth: defaults.basicAuthInfo)
         guard let request = endpoint.urlRequest else { return .emptyValue }
