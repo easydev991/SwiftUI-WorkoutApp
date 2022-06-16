da214c4f005e47c42f5898b7a19a474fdb090893
Oleg991 <o.n.eremenko@gmail.com>
2022-06-16 15:28:21 +0300

    Work in progress
    Обновил список стран и городов, добавил дефолтные координаты

--- SwiftUI-WorkoutApp/Models/City.swift
+++ SwiftUI-WorkoutApp/Models/City.swift
@@ -1,9 +1,23 @@
 import Foundation
 
 struct City: Codable, Identifiable, Hashable {
-    let id, name: String
+    let id, name, lat, lon: String
+
+    init(id: String) {
+        self.id = id
+        name = ""
+        lat = ""
+        lon = ""
+    }
+
+    init(id: String, name: String, lat: String, lon: String) {
+        self.id = id
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+    }
 
     static var defaultCity: Self {
-        .init(id: "1", name: "Москва")
+        .init(id: "1", name: "Москва", lat: "55.753215", lon: "37.622504")
     }
 }
