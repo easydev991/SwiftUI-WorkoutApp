51d5bdec26a771fa2716f1a6681490558e8868dc
Oleg991 <o.n.eremenko@gmail.com>
2023-03-18 13:37:01 +0300

    Убрал режим кнопки без текста

--- SwiftUI-WorkoutApp/DesignSystem/Sources/DesignSystem/SWButtonStyle.swift
+++ SwiftUI-WorkoutApp/DesignSystem/Sources/DesignSystem/SWButtonStyle.swift
@@ -2,64 +2,31 @@ import SwiftUI
 
 public struct SWButtonStyle: ButtonStyle {
     @Environment(\.isEnabled) private var isEnabled
-    private let mode: Mode
     private let icon: Icons.SWButton?
+    private let mode: Mode
 
-    public init(mode: SWButtonStyle.Mode, icon: Icons.SWButton? = nil) {
-        self.mode = mode
+    public init(icon: Icons.SWButton? = nil, mode: SWButtonStyle.Mode) {
         self.icon = icon
+        self.mode = mode
     }
 
     public func makeBody(configuration: Configuration) -> some View {
-        contentStack(configuration)
-            .foregroundColor(foregroundColor)
-            .padding(.vertical, 12)
-            .padding(.horizontal, 20)
-            .frame(maxWidth: .infinity)
-            .background {
-                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                    .foregroundColor(backgroundColor(configuration.isPressed))
-            }
-            .scaleEffect(configuration.isPressed ? 0.98 : 1)
-            .animation(.easeIn(duration: 0.1), value: configuration.isPressed)
-    }
-
-    @ViewBuilder
-    private func contentStack(_ configuration: Configuration) -> some View {
-        switch mode {
-        case let .filled(content):
-            switch content {
-            case let .onlyIcon(icon):
-                Image(systemName: icon.rawValue)
-            case let .iconWithText(icon):
-                HStack(spacing: 10) {
-                    Image(systemName: icon.rawValue)
-                    configuration.label
-                        .lineLimit(1)
-                        .font(.headline)
-                }
-            case .onlyText:
-                configuration.label
-                    .lineLimit(1)
-                    .font(.headline)
-            }
-        case let .tinted(content):
-            switch content {
-            case let .onlyIcon(icon):
-                Image(systemName: icon.rawValue)
-            case let .iconWithText(icon):
-                HStack(spacing: 10) {
-                    Image(systemName: icon.rawValue)
-                    configuration.label
-                        .lineLimit(1)
-                        .font(.headline)
-                }
-            case .onlyText:
-                configuration.label
-                    .lineLimit(1)
-                    .font(.headline)
-            }
+        HStack(spacing: 10) {
+            leadingIconIfNeeded
+            configuration.label
+                .lineLimit(1)
+                .font(.headline)
         }
+        .foregroundColor(foregroundColor)
+        .padding(.vertical, 12)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background {
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .foregroundColor(backgroundColor(configuration.isPressed))
+        }
+        .scaleEffect(configuration.isPressed ? 0.98 : 1)
+        .animation(.easeIn(duration: 0.1), value: configuration.isPressed)
     }
 
     @ViewBuilder
@@ -95,21 +62,14 @@ public struct SWButtonStyle: ButtonStyle {
 }
 
 public extension SWButtonStyle {
-    enum Mode {
-        case filled(_ label: LabelContent)
-        case tinted(_ label: LabelContent)
+    enum Mode: CaseIterable {
+        case filled, tinted
 
         var description: String {
             switch self {
             case .filled: return "Filled"
             case .tinted: return "Tinted"
             }
-        }
-
-        public enum LabelContent {
-            case onlyIcon(Icons.SWButton)
-            case onlyText
-            case iconWithText(Icons.SWButton)
         }
     }
 }
@@ -119,44 +79,24 @@ struct SWButtonStyle_Previews: PreviewProvider {
     static var previews: some View {
         List {
             Section("Текст без иконки") {
-                Button("Filled, only text (enabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .filled(.onlyText)))
-                Button("Filled, only text (disabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .filled(.onlyText)))
-                    .disabled(true)
-                Button("Tinted, only text (enabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .tinted(.onlyText)))
-                Button("Tinted, only text (disabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .tinted(.onlyText)))
-                    .disabled(true)
+                ForEach(SWButtonStyle.Mode.allCases, id: \.self) { mode in
+                    Button(mode.description + ", only text (enabled)") {}
+                        .buttonStyle(SWButtonStyle(mode: mode))
+                    Button(mode.description + ", only text (disabled)") {}
+                        .buttonStyle(SWButtonStyle(mode: mode))
+                        .disabled(true)
+                }
             }
             .listRowSeparator(.hidden)
             .listRowBackground(Color.clear)
             Section("Иконка с текстом") {
-                Button("Filled, icon with text (enabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .filled(.iconWithText(.message))))
-                Button("Filled, icon with text (disabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .filled(.iconWithText(.message))))
-                    .disabled(true)
-                Button("Tinted, icon with text (enabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .tinted(.iconWithText(.message))))
-                Button("Tinted, icon with text (disabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .tinted(.iconWithText(.message))))
-                    .disabled(true)
-            }
-            .listRowSeparator(.hidden)
-            .listRowBackground(Color.clear)
-            Section("Иконка без текста") {
-                Button("Filled, only icon (enabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .filled(.onlyIcon(.message))))
-                Button("Filled, only icon (disabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .filled(.onlyIcon(.message))))
-                    .disabled(true)
-                Button("Tinted, only icon (enabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .tinted(.onlyIcon(.message))))
-                Button("Tinted, only icon (disabled)") {}
-                    .buttonStyle(SWButtonStyle(mode: .tinted(.onlyIcon(.message))))
-                    .disabled(true)
+                ForEach(SWButtonStyle.Mode.allCases, id: \.self) { mode in
+                    Button(mode.description + ", icon with text (enabled)") {}
+                        .buttonStyle(SWButtonStyle(icon: .message, mode: mode))
+                    Button(mode.description + ", icon with text (disabled)") {}
+                        .buttonStyle(SWButtonStyle(icon: .message, mode: mode))
+                        .disabled(true)
+                }
             }
             .listRowSeparator(.hidden)
             .listRowBackground(Color.clear)
