74e8cfa786a8b598321a1584b745fc99a6c5dee2
Oleg991 <o.n.eremenko@gmail.com>
2023-03-19 18:54:44 +0300

    Обновил иконку в навбаре для инкогнито

--- SwiftUI-WorkoutApp/Screens/Common/IncognitoButtons/IncognitoNavbarInfoButton.swift
+++ SwiftUI-WorkoutApp/Screens/Common/IncognitoButtons/IncognitoNavbarInfoButton.swift
@@ -1,10 +1,11 @@
+import DesignSystem
 import SwiftUI
 
 /// Кнопка с навигацией на экран настроек `ProfileSettingsView`
 struct IncognitoNavbarInfoButton: View {
     var body: some View {
         NavigationLink(destination: ProfileSettingsView(mode: .incognito)) {
-            Image(systemName: "info.circle.fill")
+            Image(systemName: Icons.Button.info.rawValue)
         }
     }
 }
