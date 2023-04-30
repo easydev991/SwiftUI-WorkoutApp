e284f4d83e56c7ffa0be43c3bb0b59e7e3513296
Oleg991 <o.n.eremenko@gmail.com>
2023-04-30 10:34:00 +0300

    Доработал SectionHeaderView

--- SwiftUI-WorkoutApp/DesignSystem/Sources/DesignSystem/SectionHeaderView.swift
+++ SwiftUI-WorkoutApp/DesignSystem/Sources/DesignSystem/SectionHeaderView.swift
@@ -1,32 +1,56 @@
 import SwiftUI
 
-/// Вьюшка с заголовком для секции
-///
-/// Выглядит аналогично стандартному заголовку секции в списке
+/// Выглядит аналогично стандартному заголовку/футеру секции в списке/форме
 public struct SectionHeaderView: View {
-    private let headerText: String
-
+    private let text: String
+    private let verticalPadding: VerticalPadding
+    
     /// Инициализирует `SectionHeaderView`
-    /// - Parameter headerText: Текст заголовка секции
-    public init(_ headerText: String) {
-        self.headerText = headerText.uppercased()
+    /// - Parameters:
+    ///   - text: Текст
+    ///   - verticalPadding: Отступ сверху/снизу, по умолчанию отступ снизу
+    public init(_ text: String, verticalPadding: VerticalPadding = .bottom) {
+        self.text = text.uppercased()
+        self.verticalPadding = verticalPadding
     }
 
     public var body: some View {
-        Text(headerText)
+        Text(text)
             .foregroundColor(.swSmallElements)
             .font(.footnote)
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.leading, 12)
-            .padding(.bottom, 6)
+            .padding(vPadding.edges, vPadding.value)
     }
+    
+    private var vPadding: (edges: Edge.Set, value: CGFloat) {
+        switch verticalPadding {
+        case .top: return (.top, 12)
+        case .bottom: return (.bottom, 6)
+        }
+    }
+}
+
+public extension SectionHeaderView {
+    enum VerticalPadding { case top, bottom }
 }
 
 #if DEBUG
 struct SectionHeaderView_Previews: PreviewProvider {
+    static let headers = ["Комментарии", "Друзья", "Результаты поиска"]
+    
     static var previews: some View {
-        SectionHeaderView("Комментарии")
-            .previewLayout(.sizeThatFits)
+        VStack(spacing: 0) {
+            ForEach(headers, id: \.self) { text in
+                SectionHeaderView(text, verticalPadding: .bottom)
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.gray.opacity(0.3))
+                SectionHeaderView(text, verticalPadding: .top)
+                Spacer().frame(height: 50)
+            }
+        }
+        .padding()
+        .previewLayout(.sizeThatFits)
     }
 }
 #endif
