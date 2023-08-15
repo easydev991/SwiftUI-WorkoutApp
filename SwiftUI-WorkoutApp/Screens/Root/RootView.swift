2c6bb5c5bc6c2dd2cdef7160a8d6bb0b996c4659
Oleg991 <o.n.eremenko@gmail.com>
2023-08-15 19:19:21 +0300

    Доработка
    - Обновил картинки в таббаре
    - Убрал дефолтное заполнение картинок
    - Поправил превью
    - Сгенерировал новые скриншоты
    
    Рефактор

--- SwiftUI-WorkoutApp/Screens/Root/RootView.swift
+++ SwiftUI-WorkoutApp/Screens/Root/RootView.swift
@@ -6,60 +6,15 @@ struct RootView: View {
     @EnvironmentObject private var defaults: DefaultsService
 
     var body: some View {
-        tabView
-            .animation(.spring(), value: defaults.isAuthorized)
-    }
-}
-
-private extension RootView {
-    var tabView: some View {
         TabView(selection: $viewModel.selectedTab) {
-            SportsGroundsMapView()
-                .tabItem {
-                    tabItemView(for: .map)
-                }
-                .tag(TabViewModel.Tab.map)
-            EventsListView()
-                .tabItem {
-                    tabItemView(for: .events)
-                }
-                .tag(TabViewModel.Tab.events)
-            DialogsScreen()
-                .tabItem {
-                    tabItemView(for: .messages)
-                }
-                .tag(TabViewModel.Tab.messages)
-                .badge(defaults.unreadMessagesCount)
-            JournalsScreen()
-                .tabItem {
-                    tabItemView(for: .journal)
-                }
-                .tag(TabViewModel.Tab.journal)
-            ProfileScreen()
-                .tabItem {
-                    tabItemView(for: .profile)
-                }
-                .tag(TabViewModel.Tab.profile)
-        }
-        .navigationViewStyle(.stack)
-    }
-
-    func tabItemView(for tab: TabViewModel.Tab) -> some View {
-        Group {
-            Text(tab.title)
-            switch tab {
-            case .map:
-                Image.sportsGroundIcon
-            case .events:
-                Image(systemName: Icons.Tabbar.events.rawValue)
-            case .messages:
-                Image(systemName: Icons.Tabbar.messages.rawValue)
-            case .journal:
-                Image(systemName: Icons.Tabbar.journals.rawValue)
-            case .profile:
-                Image(systemName: Icons.Tabbar.profile.rawValue)
+            ForEach(TabViewModel.Tab.allCases, id: \.rawValue) { tab in
+                tab.screen
+                    .tabItem { tab.tabItemLabel }
+                    .tag(tab)
             }
         }
+        .navigationViewStyle(.stack)
+        .animation(.spring(), value: defaults.isAuthorized)
     }
 }
 
@@ -68,6 +23,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RootView()
             .environmentObject(DefaultsService())
+            .environmentObject(TabViewModel())
     }
 }
 #endif
