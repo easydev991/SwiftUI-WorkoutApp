ee52b7e879105a6ddf18855bdf43b84a3286df79
Oleg <51740599+OlegEremenko991@users.noreply.github.com>
2023-09-26 19:59:35 +0300

    Обновил картинки в таббаре (#99)
    * Доработка
    
    - Обновил картинки в таббаре
    - Убрал дефолтное заполнение картинок
    - Поправил превью
    - Сгенерировал новые скриншоты
    
    * Поднял версию приложения до 3.3.1

--- SwiftUI-WorkoutApp/Screens/Root/TabViewModel.swift
+++ SwiftUI-WorkoutApp/Screens/Root/TabViewModel.swift
@@ -1,4 +1,5 @@
-import Foundation
+import DesignSystem
+import SwiftUI
 
 final class TabViewModel: ObservableObject {
     @Published var selectedTab = Tab.map
@@ -7,10 +8,10 @@ final class TabViewModel: ObservableObject {
 }
 
 extension TabViewModel {
-    enum Tab: Int, Hashable {
+    enum Tab: Int, Hashable, CaseIterable {
         case map = 0, events, messages, journal, profile
 
-        var title: String {
+        private var title: String {
             switch self {
             case .map:
                 return "Площадки"
@@ -22,6 +23,46 @@ extension TabViewModel {
                 return "Дневники"
             case .profile:
                 return "Профиль"
+            }
+        }
+
+        @ViewBuilder
+        private var icon: some View {
+            switch self {
+            case .map:
+                Image.sportsGroundIcon
+            case .events:
+                Image(systemName: Icons.Tabbar.events.rawValue)
+            case .messages:
+                Image(systemName: Icons.Tabbar.messages.rawValue)
+            case .journal:
+                Image(systemName: Icons.Tabbar.journals.rawValue)
+            case .profile:
+                Image(systemName: Icons.Tabbar.profile.rawValue)
+            }
+        }
+
+        var tabItemLabel: some View {
+            Label(
+                title: { Text(title) },
+                icon: { icon }
+            )
+            .environment(\.symbolVariants, .none)
+        }
+
+        @ViewBuilder
+        var screen: some View {
+            switch self {
+            case .map:
+                SportsGroundsMapView()
+            case .events:
+                EventsListView()
+            case .messages:
+                DialogsScreen()
+            case .journal:
+                JournalsScreen()
+            case .profile:
+                ProfileScreen()
             }
         }
     }
