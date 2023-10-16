060f148bcc9aba52c7e82cdab5ea3aeffc90e909
Oleg991 <o.n.eremenko@gmail.com>
2023-10-16 22:53:39 +0300

    В процессе

--- SwiftUI-WorkoutApp/Screens/SportsGrounds/Map/SportsGroundsMapView.swift
+++ SwiftUI-WorkoutApp/Screens/SportsGrounds/Map/SportsGroundsMapView.swift
@@ -163,7 +163,8 @@ private extension SportsGroundsMapView {
 
     /// Заполняем/обновляем дефолтный список площадок
     func askForGrounds(refresh: Bool = false) async {
-        #warning("Не вызывать при возврате с детального экрана площадки")
+        #warning("Поправить исчезновение карты после refresh")
+        if !filteredGrounds.isEmpty && !refresh { return }
         guard !allSportsGrounds.isEmpty else {
             // Заполняем дефолтный список площадок контентом из `json`-файла
             do {
