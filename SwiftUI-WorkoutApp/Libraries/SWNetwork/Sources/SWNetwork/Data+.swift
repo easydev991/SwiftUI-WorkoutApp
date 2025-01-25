2b14d585036fb146dff503bf283ecbab7d2ec715
Oleg991 <o.n.eremenko@gmail.com>
2025-01-25 13:40:52 +0300

    Починил краш при логировании JSON
    Был такой краш:
    *** Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: '*** +[NSJSONSerialization dataWithJSONObject:options:error:]: Invalid top-level type in JSON write'

--- SwiftUI-WorkoutApp/Libraries/SWNetwork/Sources/SWNetwork/Data+.swift
+++ SwiftUI-WorkoutApp/Libraries/SWNetwork/Sources/SWNetwork/Data+.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension Data {
     var prettyJson: String {
-        if let object = try? JSONSerialization.jsonObject(with: self, options: [.fragmentsAllowed]),
+        if let object = try? JSONSerialization.jsonObject(with: self, options: []),
            let jsonData = try? JSONSerialization.data(withJSONObject: object, options: .prettyPrinted),
            let json = String(data: jsonData, encoding: .utf8) {
             json
