c510c33bd091efe12f3f2892e100ba5ae03bc71c
Oleg991 <o.n.eremenko@gmail.com>
2025-02-04 19:45:24 +0300

    Сделал вьюмодель для экрана с диалогами

--- SwiftUI-WorkoutApp/Screens/Messages/DialogsListScreen.swift
+++ SwiftUI-WorkoutApp/Screens/Messages/DialogsListScreen.swift
@@ -8,9 +8,8 @@ import SWUtils
 struct DialogsListScreen: View {
     @Environment(\.isNetworkConnected) private var isNetworkConnected
     @EnvironmentObject private var defaults: DefaultsService
-    @State private var dialogs = [DialogResponse]()
+    @EnvironmentObject private var viewModel: DialogsViewModel
     @State private var selectedDialog: DialogResponse?
-    @State private var isLoading = false
     @State private var indexToDelete: Int?
     @State private var openFriendList = false
     @State private var showDeleteConfirmation = false
@@ -42,7 +41,7 @@ private extension DialogsListScreen {
     var authorizedContentView: some View {
         dialogList
             .overlay { emptyContentView }
-            .loadingOverlay(if: isLoading)
+            .loadingOverlay(if: viewModel.isLoading)
             .background(Color.swBackground)
             .confirmationDialog(
                 .init(Constants.Alert.deleteDialog),
@@ -58,9 +57,6 @@ private extension DialogsListScreen {
                     friendListButton
                 }
             }
-            .onDisappear {
-                [refreshTask, deleteDialogTask].forEach { $0?.cancel() }
-            }
     }
 
     var refreshButton: some View {
@@ -71,8 +67,8 @@ private extension DialogsListScreen {
         } label: {
             Icons.Regular.refresh.view
         }
-        .opacity(showEmptyView || !DeviceOSVersionChecker.iOS16Available ? 1 : 0)
-        .disabled(isLoading)
+        .opacity(viewModel.showRefreshButton ? 1 : 0)
+        .disabled(viewModel.isLoading)
     }
 
     var friendListButton: some View {
@@ -86,20 +82,16 @@ private extension DialogsListScreen {
             Icons.Regular.plus.view
                 .symbolVariant(.circle)
         }
-        .opacity(hasFriends || !dialogs.isEmpty ? 1 : 0)
+        .opacity(hasFriends || viewModel.hasDialogs ? 1 : 0)
         .disabled(!isNetworkConnected)
     }
 
     var emptyContentView: some View {
         EmptyContentView(
             mode: .dialogs,
-            action: emptyViewAction
+            action: { openFriendList.toggle() }
         )
-        .opacity(showEmptyView ? 1 : 0)
-    }
-
-    var showEmptyView: Bool {
-        dialogs.isEmpty && !isLoading
+        .opacity(viewModel.showEmptyView ? 1 : 0)
     }
 
     @ViewBuilder
@@ -107,7 +99,7 @@ private extension DialogsListScreen {
         ZStack {
             Color.swBackground
             List {
-                ForEach(dialogs) { model in
+                ForEach(viewModel.dialogs) { model in
                     dialogListItem(model)
                         .listRowInsets(.init(top: 12, leading: 16, bottom: 12, trailing: 16))
                         .listRowBackground(Color.swBackground)
@@ -116,9 +108,9 @@ private extension DialogsListScreen {
                 .onDelete { initiateDeletion(at: $0) }
             }
             .listStyle(.plain)
-            .opacity(dialogs.isEmpty ? 0 : 1)
+            .opacity(viewModel.hasDialogs ? 1 : 0)
         }
-        .animation(.default, value: dialogs.count)
+        .animation(.default, value: viewModel.dialogs.count)
         .background(
             NavigationLink(
                 destination: lazyDestination,
@@ -130,7 +122,12 @@ private extension DialogsListScreen {
     @ViewBuilder
     var lazyDestination: some View {
         if let selectedDialog {
-            DialogScreen(dialog: selectedDialog) { markAsRead($0) }
+            DialogScreen(
+                dialog: selectedDialog,
+                markedAsReadClbk: { dialog in
+                    viewModel.markAsRead(dialog, defaults: defaults)
+                }
+            )
         }
     }
 
@@ -162,39 +159,12 @@ private extension DialogsListScreen {
         defaults.hasFriends
     }
 
-    func emptyViewAction() {
-        openFriendList.toggle()
-    }
-
-    func markAsRead(_ dialog: DialogResponse) {
-        dialogs = dialogs.map { item in
-            if item.id == dialog.id {
-                var updatedDialog = dialog
-                updatedDialog.unreadMessagesCount = 0
-                return updatedDialog
-            } else {
-                return item
-            }
-        }
-        guard dialog.unreadMessagesCount > 0,
-              defaults.unreadMessagesCount >= dialog.unreadMessagesCount
-        else { return }
-        let newValue = defaults.unreadMessagesCount - dialog.unreadMessagesCount
-        defaults.saveUnreadMessagesCount(newValue)
-    }
-
     func askForDialogs(refresh: Bool = false) async {
-        guard defaults.isAuthorized else { return }
-        if isLoading || (!dialogs.isEmpty && !refresh) { return }
-        if !refresh { isLoading = true }
         do {
-            dialogs = try await client.getDialogs()
-            let unreadMessagesCount = dialogs.map(\.unreadMessagesCount).reduce(0, +)
-            defaults.saveUnreadMessagesCount(unreadMessagesCount)
+            try await viewModel.askForDialogs(refresh: refresh, defaults: defaults)
         } catch {
             SWAlert.shared.presentDefaultUIKit(message: error.localizedDescription)
         }
-        isLoading = false
     }
 
     func initiateDeletion(at indexSet: IndexSet) {
@@ -204,17 +174,11 @@ private extension DialogsListScreen {
 
     func deleteAction(at index: Int?) {
         deleteDialogTask = Task {
-            guard let index, !isLoading else { return }
-            isLoading = true
             do {
-                let dialogID = dialogs[index].id
-                if try await client.deleteDialog(dialogID) {
-                    dialogs.remove(at: index)
-                }
+                try await viewModel.deleteDialog(at: index, defaults: defaults)
             } catch {
                 SWAlert.shared.presentDefaultUIKit(message: error.localizedDescription)
             }
-            isLoading = false
         }
     }
 }
---
+++ SwiftUI-WorkoutApp/Screens/Messages/DialogsViewModel.swift
@@ -0,0 +1,57 @@
+import Foundation
+import SWModels
+import SWNetworkClient
+import SWUtils
+
+final class DialogsViewModel: ObservableObject {
+    @Published private(set) var dialogs = [DialogResponse]()
+    @Published private(set) var isLoading = false
+    var hasDialogs: Bool { !dialogs.isEmpty }
+    var showEmptyView: Bool { !hasDialogs && !isLoading }
+    var showRefreshButton: Bool {
+        showEmptyView || !DeviceOSVersionChecker.iOS16Available
+    }
+
+    @MainActor
+    func askForDialogs(
+        refresh: Bool = false,
+        defaults: DefaultsService
+    ) async throws {
+        guard defaults.isAuthorized else { return }
+        if isLoading || (!dialogs.isEmpty && !refresh) { return }
+        if !refresh || dialogs.isEmpty { isLoading = true }
+        dialogs = try await SWClient(with: defaults).getDialogs()
+        let unreadMessagesCount = dialogs.map(\.unreadMessagesCount).reduce(0, +)
+        defaults.saveUnreadMessagesCount(unreadMessagesCount)
+        isLoading = false
+    }
+
+    @MainActor
+    func deleteDialog(at index: Int?, defaults: DefaultsService) async throws {
+        guard let index, !isLoading else { return }
+        isLoading = true
+        let dialogID = dialogs[index].id
+        if try await SWClient(with: defaults).deleteDialog(dialogID) {
+            dialogs.remove(at: index)
+        }
+        isLoading = false
+    }
+
+    @MainActor
+    func markAsRead(_ dialog: DialogResponse, defaults: DefaultsService) {
+        dialogs = dialogs.map { item in
+            if item.id == dialog.id {
+                var updatedDialog = dialog
+                updatedDialog.unreadMessagesCount = 0
+                return updatedDialog
+            } else {
+                return item
+            }
+        }
+        guard dialog.unreadMessagesCount > 0,
+              defaults.unreadMessagesCount >= dialog.unreadMessagesCount
+        else { return }
+        let newValue = defaults.unreadMessagesCount - dialog.unreadMessagesCount
+        defaults.saveUnreadMessagesCount(newValue)
+    }
+}
