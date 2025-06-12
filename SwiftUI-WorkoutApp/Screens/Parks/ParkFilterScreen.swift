ba9bc23154c9344e940faf07244225eb726a47d3
Oleg <51740599+easydev991@users.noreply.github.com>
2025-06-12 11:40:45 +0300

    Доработка фильтров площадок

--- SwiftUI-WorkoutApp/Screens/Parks/ParkFilterScreen.swift
+++ SwiftUI-WorkoutApp/Screens/Parks/ParkFilterScreen.swift
@@ -3,12 +3,17 @@ import SwiftUI
 import SWModels
 
 struct ParkFilterScreen: View {
+    @Environment(\.dismiss) private var dismiss
+    /// Фильтр на родительском экране
     @Binding private var filter: Model
+    /// Локальный фильтр
+    @State private var localFilter: Model
     private let allSizes = ParkSize.allCases
     private let allGrades = ParkGrade.allCases
 
     init(filter: Binding<Model>) {
         self._filter = filter
+        self._localFilter = .init(initialValue: filter.wrappedValue)
     }
 
     var body: some View {
@@ -34,7 +39,10 @@ struct ParkFilterScreen: View {
                             }
                         }
                     }
-                    resetFilterButton
+                    VStack(spacing: 12) {
+                        resetFilterButton
+                        applyFilterButton
+                    }
                 }
                 .padding([.top, .horizontal])
             }
@@ -56,47 +64,58 @@ extension ParkFilterScreen {
 private extension ParkFilterScreen {
     func buttonFor(_ size: ParkSize) -> some View {
         Button {
-            if filter.size.contains(size) {
-                guard filter.size.count > 1 else { return }
-                filter.size = filter.size.filter { $0 != size }
+            if localFilter.size.contains(size) {
+                guard localFilter.size.count > 1 else { return }
+                localFilter.size = localFilter.size.filter { $0 != size }
             } else {
-                filter.size.append(size)
+                localFilter.size.append(size)
             }
         } label: {
             TextWithCheckmarkRowView(
                 text: size.description,
-                isChecked: filter.size.contains(size)
+                isChecked: localFilter.size.contains(size)
             )
         }
     }
 
     func buttonFor(_ grade: ParkGrade) -> some View {
         Button {
-            if filter.grade.contains(grade) {
-                guard filter.grade.count > 1 else { return }
-                filter.grade = filter.grade.filter { $0 != grade }
+            if localFilter.grade.contains(grade) {
+                guard localFilter.grade.count > 1 else { return }
+                localFilter.grade = localFilter.grade.filter { $0 != grade }
             } else {
-                filter.grade.append(grade)
+                localFilter.grade.append(grade)
             }
         } label: {
             TextWithCheckmarkRowView(
                 text: grade.description,
-                isChecked: filter.grade.contains(grade)
+                isChecked: localFilter.grade.contains(grade)
             )
         }
     }
 
     var resetFilterButton: some View {
-        Button("Сбросить фильтры") {
-            filter = .init()
+        Button("Сбросить") {
+            localFilter = .init()
+        }
+        .buttonStyle(SWButtonStyle(mode: .tinted, size: .large))
+        .disabled(!localFilter.isEdited)
+    }
+
+    var applyFilterButton: some View {
+        Button("Применить") {
+            filter = localFilter
+            dismiss()
         }
         .buttonStyle(SWButtonStyle(mode: .filled, size: .large))
-        .disabled(!filter.isEdited)
+        .disabled(localFilter == filter)
     }
 }
 
 #if DEBUG
+@available(iOS 17, *)
 #Preview {
-    ParkFilterScreen(filter: .constant(.init()))
+    @Previewable @State var filter = ParkFilterScreen.Model()
+    ParkFilterScreen(filter: $filter)
 }
 #endif
